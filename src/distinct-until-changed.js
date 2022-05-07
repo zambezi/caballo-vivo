dff5f9b429103f64136232f48e6d0efedfbfdf00
Yerlan Yar-Mukhamedov <yerlanyr@gmail.com>
2022-05-08 00:31:54 +0600

    Refactor distinct until changed

--- src/distinct-until-changed.js
+++ src/distinct-until-changed.js
@@ -1,11 +1,6 @@
 import { distinctUntilChanged as d } from 'rxjs/operators'
 import { equals } from 'ramda'
 
-export default function distinctUntilChanged(source) {
-  return source.pipe(d(diligentEquals))
-}
+const distinctUntilChanged = d(equals)
 
-function diligentEquals(a, b) {
-  if (typeof a.equals === 'function') return a.equals(b)
-  return equals(a, b)
-}
+export default distinctUntilChanged
